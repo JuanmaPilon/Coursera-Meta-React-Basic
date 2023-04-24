08c61996a0dcb140c91b7933dc127b527e3a77d6
Juanma <juanmartin.pilon@gmail.com>
2023-04-24 17:22:22 -0300

    Agregando un par de cambios y haciendo pruebas a medidas que realizo ejercicios

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import pocahontas from "./pocahontas-carito.jpeg" 
 
+function Main (props) {
+  return <h2>Soy {props.name} (La pocahontas)</h2>
+};
 
-function Header() {
-  return <h1>Hello World</h1>;
+function ImageComp () {
+  const caritoPic = <img src={pocahontas} alt='ImagenCarito'/>;
+  return caritoPic;
+}
+
+function Header(props) {
+  return <h1>Hello, bienvenidos a mi pagina, {props.apodo}</h1>;
 }
 
 function App() {
-  return <Header />;
+  return (
+  <div>
+    <Header apodo = "la roba kills!"/>
+    <Main name = "Carito"/>
+    <ImageComp />
+  </div>
+  );
 }
 
 export default App;
